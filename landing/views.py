64a4559a4ee30c30dc184207239d3ff291a03aec
jonathan-escritorio <joni92@gmail.com>
2024-12-23 05:09:25 -0300

    colacion  de validacion

--- landing/views.py
+++ landing/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib import messages
 
+
+usertest = "admin"
+passwordtest = "admin"  
+
 def inicio(request):
     return render(request, 'inicio.html')
 
@@ -9,10 +13,8 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-            auth_login(request, user)
+    
+        if username == usertest and password == passwordtest:
             return redirect('landing:inicio')  
         else:
             messages.error(request, 'Usuario o contraseña incorrectos')
